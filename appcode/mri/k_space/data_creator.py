Fix the precision issue

appcode/mri/k_space/data_creator.py
@@ -25,7 +25,7 @@ class DataCreator:
         :return:
         """
         self.mri_data_base = mri_data_base
-        self.base_out_path = output_path
+        self.base_out_path = os.path.join(output_path, 'base')
         self.axial_limits = axial_limits
 
     def create_examples(self, item='all'):
@@ -45,7 +45,7 @@ class DataCreator:
             # Set output path and create dir
             tt = self.mri_data_base.info["train_test_list"][case_name]
             out_path = os.path.join(self.base_out_path, tt, case_name)
-            os.mkdir(out_path)
+            os.makedirs(out_path)
             counter = 0
 
             # Read source data and create k_space + dummy image
@@ -60,8 +60,8 @@ class DataCreator:
             # For each Z in axial limits, create masks and dump examples
             for z in range(self.axial_limits[0], self.axial_limits[1]+1):
                 k_space_2d = k_space_3d[:, :, z]
-                k_space_real = k_space_2d.real.astype(np.int16)
-                k_space_imag = k_space_2d.imag.astype(np.int16)
+                k_space_real = k_space_2d.real
+                k_space_imag = k_space_2d.imag
                 dummy_image_2d = dummy_image_3d[:, :, z]
 
                 for mask_type in range(0,2):

appcode/mri/k_space/files_info.py
@@ -9,8 +9,8 @@ with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'meta_data_p
     meta_data_params = json.load(f)
     sizeof_meta_data = meta_data_params["sizeof_meta_data"]
 
-files_info = {"k_space_real":  {"width": 256, "height": 256, "channels": 1, "dtype": np.int16},
-              "k_space_imag":  {"width": 256, "height": 256, "channels": 1, "dtype": np.int16},
+files_info = {"k_space_real":  {"width": 256, "height": 256, "channels": 1, "dtype": np.float32},
+              "k_space_imag":  {"width": 256, "height": 256, "channels": 1, "dtype": np.float32},
               "image":         {"width": 256, "height": 256, "channels": 1, "dtype": np.int16},
               "mask":          {"width": 256, "height": 256, "channels": 1, "dtype": np.uint8},
               "meta_data":     {"width": sizeof_meta_data, "height": 1,   "channels": 1, "dtype": np.float32}}

appcode/mri/k_space/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+EPS = 0.01
+
 def get_dummy_k_space_and_image(img):
     """
     Get an MRI image and return k_space and dummy image
@@ -7,21 +9,29 @@ def get_dummy_k_space_and_image(img):
     :return: k_space, img
     """
 
-    k_space = np.zeros_like(img) + 0j
+    k_space = (np.zeros_like(img) + 0j).astype('complex64')
     dummy_img = np.zeros_like(img)
 
     if len(img.shape) == 2:
         # Create k-space from image, assuming this is the fully-sampled k-space.
-        k_space = np.fft.fftshift(np.fft.fft2(img))
+        k_space = np.fft.fftshift(np.fft.fft2(img)).astype('complex64')
+
+        # Clipping
+        k_space.real[np.where(np.abs(k_space.real) < EPS)] = EPS
+        k_space.imag[np.where(np.abs(k_space.imag) < EPS)] = EPS
 
         # Reconstruct image
-        dummy_img = np.abs(np.fft.ifft2(k_space))
+        dummy_img = np.abs(np.fft.ifft2(k_space)).astype(np.int16)
     else:
         for i in range(0, img.shape[2]):
             # Create k-space from image, assuming this is the fully-sampled k-space.
-            k_space[:,:,i] = np.fft.fftshift(np.fft.fft2(img[:,:,i]))
+            k_space[:,:,i] = (np.fft.fftshift(np.fft.fft2(img[:,:,i]))).astype('complex64')
+
+            # Clipping
+            k_space[:,:,i].real[np.where(np.abs(k_space[:,:,i].real) < EPS)] = EPS
+            k_space[:,:,i].imag[np.where(np.abs(k_space[:,:,i].imag) < EPS)] = EPS
 
             # Reconstruct image
-            dummy_img[:,:,i] = np.abs(np.fft.ifft2(k_space[:,:,i]))
+            dummy_img[:,:,i] = np.abs(np.fft.ifft2(k_space[:,:,i])).astype(np.int16)
 
     return k_space, dummy_img

appcode/mri/unitest.py
@@ -18,7 +18,12 @@ data_source = MriDataBase('SchizReg')
 data_example = data_source.get_source_data(data_source.items[0])
 dat = data_example['img'][0]
 original_image = dat[:,:,50]
-k_space, dummy_image = get_dummy_k_space_and_image(dat[:,:,50])
+# k_space, dummy_image = get_dummy_k_space_and_image(dat[:,:,50])
+k_space_all, dummy_image_all = get_dummy_k_space_and_image(dat)
+
+
+k_space = k_space_all[:,:,50]
+dummy_image = dummy_image_all[:,:,50]
 
 if to_show:
 
@@ -30,10 +35,12 @@ if to_show:
     imshow(original_image, ax=ax[0][0], fig=fig)
 
     ax[0][1].set_title('Log-k_space')
-    imshow(np.log(1+np.abs(k_space)), ax=ax[0][1], fig=fig)
+    imshow(np.log(np.abs(k_space)), ax=ax[0][1], fig=fig)
 
     ax[1][0].set_title('Dummy Image')
     imshow(dummy_image, ax=ax[1][0], fig=fig)
 
     ax[1][1].set_title('Diff')
-    imshow(np.log(np.abs(original_image - dummy_image)), ax=ax[1][1], fig=fig)
+    imshow(np.abs(original_image - dummy_image), ax=ax[1][1], fig=fig)
+
+a = 1