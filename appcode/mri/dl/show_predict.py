3ddd2dc3b25b8443e6b1fc6fc90823009785b64e
shohad25 <shohad25@gmail.com>
2016-07-07 22:00:57 +0300

    Upate show predict

--- appcode/mri/dl/show_predict.py
+++ appcode/mri/dl/show_predict.py
@@ -10,7 +10,7 @@ to_show = True
 
 # k space data set
 data_dir = '/sheard/Ohad/thesis/data/SchizData/SchizReg/train/24_05_2016/shuffle/'
-file_names = ['k_space_real', 'k_space_real_gt', 'mask']
+file_names = ['k_space_real', 'k_space_real_gt', 'mask', 'image_gt']
 data_set = KspaceDataSet(data_dir, file_names, stack_size=50, shuffle=False)
 predict_dir = '/media/ohadsh/sheard/Ohad/thesis/data/SchizData/SchizReg/train/24_05_2016/runs/2016_02_06/predict/test/'
 tt = 'test'
@@ -28,19 +28,25 @@ x_upscaled = f_x_upscaled.read(mini_batch_size, reshaped=True)
 if to_show:
     fig, ax = plt.subplots(nrows=2, ncols=2)
 
+    fig2, ax2 = plt.subplots(nrows=2, ncols=2)
+
     for i in range(0, mini_batch_size):
 
         ax[0][0].set_title('k-space - ground truth')
-        imshow(batch["k_space_real_gt"][i,:,:], ax=ax[0][0], fig=fig)
+        imshow(np.log(0.1+np.abs(batch["k_space_real_gt"][i,:,:])), ax=ax[0][0], fig=fig)
 
-        ax[0][1].set_title('k-space - subsampeld')
-        imshow(batch["k_space_real"][i,:,:], ax=ax[0][1], fig=fig)
+        ax[0][1].set_title('k-space - subsample')
+        imshow(np.log(0.1+np.abs(batch["k_space_real"][i,:,:])), ax=ax[0][1], fig=fig)
 
         ax[1][0].set_title('k-space - interpolation')
-        imshow(x_upscaled[i,:,:].transpose(), ax=ax[1][0], fig=fig)
+        imshow(np.log(0.1+np.abs(x_upscaled[i,:,:].transpose())), ax=ax[1][0], fig=fig)
 
         ax[1][1].set_title('k-space - predicted')
-        imshow(y_pred[i,:,:].transpose(), ax=ax[1][1], fig=fig)
+        imshow(np.log(0.1+np.abs(y_pred[i,:,:].transpose())), ax=ax[1][1], fig=fig)
 
+        ax2[0][0].set_title('Original Image')
+        imshow(batch['image_gt'][i,:,:], ax=ax2[0][0], fig=fig2)
+        ax2[0][1].set_title('Sampling Mask')
+        imshow(batch['mask'][i,:,:], ax=ax2[0][1], fig=fig2)
         plt.waitforbuttonpress(timeout=-1)
         print "i = " + str(i)
