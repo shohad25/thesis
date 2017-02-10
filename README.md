6ecfd16a91c4f910e22f8bab6bec0d9664d78cbd
shohad25 <shohad25@gmail.com>
2017-02-10 15:02:15 +0200

    Update GAN

--- README.md
+++ README.md
@@ -9,23 +9,3 @@ Information for report:
 
 50 cases: 35 trainin 15 testing
 MRI, 1.5 Tesla
-
-<<<<<<< HEAD
-0.014654596
-(Pdb) np.mean(aa**2)
-0.001035507
-=======
-
-'session_error': array(0, dtype=int16), 'extents': array(0, dtype=int32), 'sizeof_hdr': array(348, dtype=int32), 'srow_x': array([ 0.9375,  0.    , -0.    , -0.    ], dtype=float32), 'srow_y': array([ 0.    ,  0.9375, -0.    , -0.    ], dtype=float32), 'pixdim': array([ 1.    ,  0.9375,  0.9375,  1.5   ,  1.    ,  1.    ,  1.    ,  1.    ], dtype=float32), 'slice_start': array(0, dtype=int16), 'xyzt_units': array(2, dtype=uint8), 'cal_max': array(0.0, dtype=float32), 'intent_p1': array(0.0, dtype=float32), 'intent_p2': array(0.0, dtype=float32), 'intent_p3': array(0.0, dtype=float32), 'qoffset_x': array(-0.0, dtype=float32), 'intent_code': array(0, dtype=int16), 'qoffset_z': array(0.0, dtype=float32), 'sform_code': array(1, dtype=int16), 'cal_min': array(0.0, dtype=float32), 'scl_slope': array(nan, dtype=float32), 'slice_code': array(0, dtype=uint8), 'slice_duration': array(0.0, dtype=float32), 'quatern_b': array(0.0, dtype=float32), 'vox_offset': array(0.0, dtype=float32), 'bitpix': array(16, dtype=int16), 'descrip': array('', 
-      dtype='|S80'), 'glmin': array(0, dtype=int32), 'dim_info': array(0, dtype=uint8), 'glmax': array(0, dtype=int32), 'quatern_c': array(0.0, dtype=float32), 'data_type': array('', 
-      dtype='|S10'), 'aux_file': array('none', 
-      dtype='|S24'), 'intent_name': array('', 
-      dtype='|S16'), 'srow_z': array([ 0. ,  0. ,  1.5,  0. ], dtype=float32), 'db_name': array('', 
-      dtype='|S18'), 'scl_inter': array(nan, dtype=float32), 'quatern_d': array(0.0, dtype=float32), 'dim': array([  3, 256, 256, 124,   1,   1,   1,   1], dtype=int16), 'magic': array('ni1', 
-      dtype='|S4'), 'datatype': array(4, dtype=int16), 'regular': array('r', 
-      dtype='|S1'), 'slice_end': array(0, dtype=int16), 'qform_code': array(1, dtype=int16), 'qoffset_y': array(-0.0, dtype=float32), 'toffset': array(0.0, dtype=float32)}
-      
-      
-Best training so far
-0.0012
->>>>>>> d09f535b50f0839c72f00ce57e60ff2098f3f4d6
--- appcode/mri/dl/gan/main_gan.py
+++ appcode/mri/dl/gan/main_gan.py
@@ -10,13 +10,14 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 from appcode.mri.k_space.k_space_data_set import KspaceDataSet
-from appcode.mri.dl.multi_channel.k_space_multi_channel import KSpaceSuperResolutionMC
+from appcode.mri.dl.gan.k_space_gan import KSpaceSuperResolutionGAN
 from common.deep_learning.helpers import *
 import copy
 import os
 import datetime
 import argparse
 import json
+from collections import defaultdict
 
 # k space data set on loca SSD
 base_dir = '/home/ohadsh/work/data/SchizReg/24_05_2016/'
@@ -25,15 +26,15 @@ file_names = {'x_r': 'k_space_real', 'x_i': 'k_space_imag', 'y_r': 'k_space_real
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_integer('max_steps', 5000000, 'Number of steps to run trainer.')
-flags.DEFINE_float('learning_rate', 1e-4, 'Initial learning rate.')
+flags.DEFINE_float('learning_rate', 1e-6, 'Initial learning rate.')
 flags.DEFINE_float('regularization_weight', 5e-4, 'L2 Norm regularization weight.')
 flags.DEFINE_integer('mini_batch_size', 10, 'Size of mini batch')
 flags.DEFINE_integer('mini_batch_predict', 50, 'Size of mini batch for predict')
 
-flags.DEFINE_integer('print_test', 10000, 'Print test frequency')
-flags.DEFINE_integer('print_train', 1000, 'Print train frequency')
-# flags.DEFINE_integer('print_test', 100, 'Print test frequency')
-# flags.DEFINE_integer('print_train', 10, 'Print train frequency')
+# flags.DEFINE_integer('print_test', 10000, 'Print test frequency')
+# flags.DEFINE_integer('print_train', 1000, 'Print train frequency')
+flags.DEFINE_integer('print_test', 100, 'Print test frequency')
+flags.DEFINE_integer('print_train', 100, 'Print train frequency')
 
 flags.DEFINE_boolean('to_show', False, 'View data')
 
@@ -80,34 +81,39 @@ def feed_data(data_set, x_input, y_input, train_phase, tt='train', batch_size=10
     sigma_i = np.sqrt(np.float32(data_factors['variance'][file_names['x_i']]))
     norm_i = lambda x: (x - mu_i) / sigma_i
 
+    y_in = np.concatenate((norm_r(next_batch[file_names['y_r']][:, :, :, np.newaxis]),
+                                     norm_i(next_batch[file_names['y_i']][:, :, :, np.newaxis])), 3)
+    # d_in = y_in.copy()
     # Feed input as multi-channel: [0: real, 1: imaginary]
     feed = {x_input: np.concatenate((norm_r(next_batch[file_names['x_r']][:, :, :, np.newaxis]),
                                      norm_i(next_batch[file_names['x_i']][:, :, :, np.newaxis])), 3),
-            y_input: np.concatenate((norm_r(next_batch[file_names['y_r']][:, :, :, np.newaxis]),
-                                     norm_i(next_batch[file_names['y_i']][:, :, :, np.newaxis])), 3),
+            y_input: y_in,
             train_phase: t_phase
             }
     return feed
 
 
-def run_evaluation(sess, feed, eval_op, step, summary_op, writer, tt):
+def run_evaluation(sess, feed, net, step, writer, tt):
     """
-    Run evaluation and save checkpoint
-    :param sess: tf session
-    :param feed: dictionary feed
-    :param step: global step
-    :param summary_op:
-    :param eval_op:
+
+    :param sess:
+    :param feed:
+    :param net:
+    :param step:
     :param writer:
-    :param tt: TRAIN / TEST
+    :param tt:
     :return:
     """
-    result = sess.run([summary_op, eval_op], feed_dict=feed)
-    summary_str = result[0]
-    acc = result[1]
-    writer.add_summary(summary_str, step)
-    print('%s:  Time: %s , Accuracy at step %s: %s' % (tt, datetime.datetime.now(), step, acc))
-    logfile.writelines('%s: Time: %s , Accuracy at step %s: %s\n' % (tt, datetime.datetime.now(), step, acc))
+    m_op_g = tf.summary.merge_all(key='G')
+    m_op_d = tf.summary.merge_all(key='D')
+
+    r_g, r_d, loss_d_fake, loss_d_real, loss_d, loss_g = sess.run([m_op_g, m_op_d, net.d_loss_fake, net.d_loss_real,
+                                                                   net.d_loss, net.g_loss], feed_dict=feed)
+    writer['G'].add_summary(r_g, step)
+    writer['D'].add_summary(r_d, step)
+
+    print('%s:  Time: %s , Loss at step %s: D: %s, G: %s' % (tt, datetime.datetime.now(), step, loss_d, loss_g))
+    logfile.writelines('%s: Time: %s , Accuracy at step %s: D: %s, G: %s\n' % (tt, datetime.datetime.now(), step, loss_d, loss_g))
     logfile.flush()
 
 
@@ -130,8 +136,9 @@ def load_graph():
     # Init inputs as placeholders
     x_input = tf.placeholder(tf.float32, shape=[None] + list(DIMS_IN), name='x_input')
     y_input = tf.placeholder(tf.float32, shape=[None] + list(DIMS_OUT), name='y_input')
+    # d_input = tf.placeholder(tf.float32, shape=[None] + list(DIMS_OUT), name='d_input')
     train_phase = tf.placeholder(tf.bool, name='phase_train')
-    network = KSpaceSuperResolutionMC(input=x_input, labels=y_input, dims_in=DIMS_IN,
+    network = KSpaceSuperResolutionGAN(input=x_input, labels=y_input, dims_in=DIMS_IN,
                                       dims_out=DIMS_OUT, batch_size=FLAGS.mini_batch_size,
                                       reg_w=FLAGS.regularization_weight, train_phase=train_phase)
     network.build(FLAGS)
@@ -153,40 +160,56 @@ def train_model(mode, checkpoint=None):
 
     sess = tf.Session()
     init = tf.global_variables_initializer()
-    writer = tf.summary.FileWriter(os.path.join(FLAGS.train_dir, 'train'), sess.graph)
-    writer_test = tf.summary.FileWriter(os.path.join(FLAGS.train_dir, 'test'), sess.graph)
+
+    writer = defaultdict(dict)
+    writer['train']['D'] = tf.summary.FileWriter(os.path.join(FLAGS.train_dir, 'train', 'D'), sess.graph)
+    writer['train']['G'] = tf.summary.FileWriter(os.path.join(FLAGS.train_dir, 'train', 'G'), sess.graph)
+
+    writer['test']['D'] = tf.summary.FileWriter(os.path.join(FLAGS.train_dir, 'test', 'D'), sess.graph)
+    writer['test']['G'] = tf.summary.FileWriter(os.path.join(FLAGS.train_dir, 'test', 'G'), sess.graph)
 
     if mode == 'resume':
         # saver.restore(sess, checkpoint)
         saver.restore(sess, tf.train.latest_checkpoint(checkpoint))
     else:
         sess.run(init)
+
     # Train the model, and feed in test data and record summaries every 10 steps
     for i in range(FLAGS.max_steps):
 
+        #
         if i % FLAGS.print_test == 0:
             # Record summary data and the accuracy
             feed = feed_data(data_set, net.input, net.labels, net.train_phase,
                              tt='test', batch_size=FLAGS.mini_batch_size)
 
             if len(feed[net.input]):
-                run_evaluation(sess, feed, step=i, summary_op=merged, eval_op=net.evaluation, writer=writer_test, tt='TEST')
+                run_evaluation(sess, feed, step=i, net=net, writer=writer['test'], tt='TEST')
                 save_checkpoint(sess=sess, saver=saver, step=i)
 
         else:
             # Training
             feed = feed_data(data_set, net.input, net.labels, net.train_phase,
                              tt='train', batch_size=FLAGS.mini_batch_size)
+            # sess.run([merged], feed_dict=feed)
             if len(feed[net.input]):
-                # _, dbg, loss_value = sess.run([net.train_step, net.debug, net.loss], feed_dict=feed)
-                _,loss_value = sess.run([net.train_step, net.loss], feed_dict=feed)
+
+                # Update D network
+                _, d_loss_fake, d_loss_real, d_loss = \
+                    sess.run([net.train_op_d, net.d_loss_fake, net.d_loss_real, net.d_loss], feed_dict=feed)
+
+                # Update G network
+                _, g_loss = sess.run([net.train_op_g, net.g_loss], feed_dict=feed)
+
+                # Run g_optim twice to make sure that d_loss does not go to zero
+                # (different from paper)
+                _, g_loss = sess.run([net.train_op_g, net.g_loss], feed_dict=feed)
+
             if i % FLAGS.print_train == 0:
-                run_evaluation(sess, feed, step=i, summary_op=merged, eval_op=net.evaluation, writer=writer, tt='TRAIN')
+                run_evaluation(sess, feed, step=i, net=net, writer=writer['train'], tt='TRAIN')
             # import pdb
             # pdb.set_trace()
             # print(dbg[0].mean(), dbg[1].mean())
-
-
 
     logfile.close()
 
@@ -210,9 +233,6 @@ def evaluate_checkpoint(tt='test', checkpoint=None, output_file=None, output_fil
     sess = tf.Session()
     # saver.restore(sess, checkpoint)
     saver.restore(sess, tf.train.latest_checkpoint(checkpoint))
-    # all_vars = tf.trainable_variables()
-    # for v in all_vars:
-    #     print(v.name)
 
     data_set_tt = getattr(data_set, tt)
 
@@ -229,7 +249,7 @@ def evaluate_checkpoint(tt='test', checkpoint=None, output_file=None, output_fil
             feed = feed_data(data_set, net.input, net.labels, net.train_phase,
                              tt='train', batch_size=FLAGS.mini_batch_size)
             if len(feed[net.input]):
-                predict, result, x_interp = sess.run([net.predict, net.evaluation, net.x_input_upscale], feed_dict=feed)
+                predict, result, x_interp = sess.run([net.predict_g, net.evaluation, net.x_input_upscale], feed_dict=feed)
                 all_acc.append(np.array(result))
                 print('Time: %s , Accuracy for mini_batch is: %s' % (datetime.datetime.now(), result))
                 if output_file is not None:
