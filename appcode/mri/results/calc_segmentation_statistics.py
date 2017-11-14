82ad519cbb0e046e70cb1fb1d4944566094235ef
ohadsh <shohad25@gmail.com>
2017-11-14 19:08:40 +0200

    fix segemnetaion code

--- appcode/mri/results/calc_segmentation_statistics.py
+++ appcode/mri/results/calc_segmentation_statistics.py
@@ -5,16 +5,24 @@ import argparse
 import nibabel as nib
 NII_SUFFIX = '.nii.gz'
 SEG_SUFFIX = '_seg'
+BRAIN_SUFFIX = '_brain'
+CLASSES = [0,1,2,3]
 
 
-def calc_segmentation_statistics(data_dir, num_of_cases=-1, suffixes=None):
+def calc_segmentation_statistics(data_dir, num_of_cases=-1, suffixes=None, brain_only=True):
     """ Go over segmentation results and calc
     TPR, TNR, ACC, DICE score
     :param data_dir:
     :param num_of_cases:
     :param suffixes:
+    :param brain_only: calculate on brain only
     :return:
     """
+    if brain_only:
+        seg_suffix_use = BRAIN_SUFFIX+SEG_SUFFIX
+    else:
+        seg_suffix_use = SEG_SUFFIX
+
     suffixes = eval(suffixes) if suffixes is not None else None
     num_of_cases = 1000000000 if num_of_cases == -1 else num_of_cases
     sub_dirs = os.listdir(data_dir)
@@ -22,8 +30,10 @@ def calc_segmentation_statistics(data_dir, num_of_cases=-1, suffixes=None):
     FPR = {suffix: [] for suffix in suffixes}
     DICE = {suffix: [] for suffix in suffixes}
     for sub_dir in sub_dirs:
-        if sub_dirs.index(sub_dir) > num_of_cases-1 or not os.path.isdir(sub_dir):
+        if sub_dirs.index(sub_dir) > num_of_cases - 1:
             break
+        if not os.path.isdir(os.path.join(data_dir, sub_dir)):
+            continue
         path_sub_dir = os.path.join(data_dir, sub_dir)
         all_files_in_dir = os.listdir(path_sub_dir)
         suffix_to_nifti = {}
@@ -32,7 +42,7 @@ def calc_segmentation_statistics(data_dir, num_of_cases=-1, suffixes=None):
         # Collect all relevant files for this case
         for file_type in all_files_in_dir:
             for suffix in suffixes:
-                if file_type.endswith(sub_dir+suffix+SEG_SUFFIX+NII_SUFFIX):
+                if file_type.endswith(sub_dir+suffix+seg_suffix_use+NII_SUFFIX):
                     suffix_to_nifti[suffix] = nib.load(os.path.join(path_sub_dir, file_type))
 
         if '' in suffix_to_nifti.keys():
@@ -41,24 +51,27 @@ def calc_segmentation_statistics(data_dir, num_of_cases=-1, suffixes=None):
                 TPR[suffix].append(tpr_case[suffix])
                 FPR[suffix].append(fpr_case[suffix])
                 DICE[suffix].append(dice_case[suffix])
+
     # Print results
+    print('Results\n')
     for suffix in TPR.keys():
         tpr_array = np.array(TPR[suffix])
         fpr_array = np.array(FPR[suffix])
         dice_array = np.array(DICE[suffix])
 
-        tpr_mean = np.mean(tpr_array)
-        fpr_mean = np.mean(fpr_array)
-        dice_mean = np.mean(dice_array)
+        tpr_mean = np.mean(tpr_array, axis=0)
+        fpr_mean = np.mean(fpr_array, axis=0)
+        dice_mean = np.mean(dice_array, axis=0)
 
-        tpr_std = np.std(tpr_array)
-        fpr_std = np.std(fpr_array)
-        dice_std = np.std(dice_array)
+        tpr_std = np.std(tpr_array, axis=0)
+        fpr_std = np.std(fpr_array, axis=0)
+        dice_std = np.std(dice_array, axis=0)
 
         print("Version - %s" % suffix)
-        print("TPR = %f, %f" % (tpr_mean, tpr_std))
-        print("FPR = %f, %f" % (fpr_mean, fpr_std))
-        print("DICE = %f, %f" % (dice_mean, dice_std))
+        # Print total result
+        print("TPR  = %f, %f ||| %s , %s" % (tpr_mean.mean(), tpr_std.mean(), tpr_mean, tpr_std))
+        print("FPR  = %f, %f ||| %s , %s" % (fpr_mean.mean(), fpr_std.mean(), fpr_mean, fpr_std))
+        print("DICE = %f, %f ||| %s , %s\n" % (dice_mean.mean(), dice_std.mean(), dice_mean, dice_std))
 
 
 def calc_statistics(suffix_to_nifti):
@@ -67,7 +80,44 @@ def calc_statistics(suffix_to_nifti):
     :param suffix_to_nifti:
     :return:
     """
-    CLASS = 3
+    ref = suffix_to_nifti[''].get_data()
+
+    tpr = {}
+    fpr = {}
+    dice = {}
+    num_of_cls = len(CLASSES)
+    for (ver_name, ver_nifti) in suffix_to_nifti.iteritems():
+        if ver_name == '':
+            tpr[ver_name] = num_of_cls*[1.0]
+            fpr[ver_name] = num_of_cls*[0.0]
+            dice[ver_name] = num_of_cls*[1.0]
+            continue
+        data = ver_nifti.get_data()
+        tpr_temp = []
+        fpr_temp = []
+        dice_temp = []
+        for cls in CLASSES:
+            # One class binary
+            ref_cls = ref == cls
+            data_cls = data == cls
+            # Calc values
+            tpr_temp.append(float(np.sum(data_cls == ref_cls)) / len(data_cls.ravel()))
+            fpr_temp.append(float(np.sum(data_cls != ref_cls)) / len(data_cls.ravel()))
+            dice_temp.append(calc_dice(data_cls, ref_cls))
+
+        tpr[ver_name] = tpr_temp
+        fpr[ver_name] = fpr_temp
+        dice[ver_name] = dice_temp
+    return tpr, fpr, dice
+
+
+def calc_statistics_class(suffix_to_nifti, CLASS=3):
+    """
+    Get dictionary - suffix to nifti, return statistics
+    :param suffix_to_nifti:
+    :param CLASS:
+    :return:
+    """
     ref = suffix_to_nifti[''].get_data()
     ref = ref == CLASS
     tpr = {}
@@ -126,6 +176,7 @@ if __name__ == '__main__':
     parser.add_argument('--data_dir', dest='data_dir', type=str, help='data directory')
     parser.add_argument('--num_of_cases', dest='num_of_cases', type=int, default=-1,  help='number of cases')
     parser.add_argument('--suffixes', dest='suffixes', default=None, type=str, help='suffixes')
+    parser.add_argument('--brain_only', dest='brain_only', default=True, type=bool, help='brain only')
     args = parser.parse_args()
 
-    calc_segmentation_statistics(args.data_dir, args.num_of_cases, args.suffixes)
+    calc_segmentation_statistics(args.data_dir, args.num_of_cases, args.suffixes, args.brain_only)
--- appcode/mri/results/create_segmentation_commands.py
+++ appcode/mri/results/create_segmentation_commands.py
@@ -3,6 +3,9 @@ import numpy as np
 import os
 import argparse
 NII_SUFFIX = '.nii.gz'
+SEG_CMD = '$FSLDIR/bin/fast -n 3 -I 4 '
+BET_CMD = '$FSLDIR/bin/bet'
+BET_PARAMS = '-f 0.5 -g 0'
 
 
 def create_segmentation_commands(data_dir, num_of_cases=-1, suffixes=None):
@@ -15,22 +18,35 @@ def create_segmentation_commands(data_dir, num_of_cases=-1, suffixes=None):
     suffixes = eval(suffixes) if suffixes is not None else None
     num_of_cases = 1000000000 if num_of_cases == -1 else num_of_cases
     sub_dirs = os.listdir(data_dir)
-    with open(os.path.join(data_dir, 'segmentations.sh'), 'w') as f:
+    cmd_file_out_path = os.path.join(data_dir, 'segmentations.sh')
+    with open(cmd_file_out_path, 'w') as f:
         f.write('setFSL\n')
         cmds = []
         for sub_dir in sub_dirs:
-            if sub_dirs.index(sub_dir) > num_of_cases - 1 or not os.path.isdir(sub_dir):
+            if sub_dirs.index(sub_dir) > num_of_cases - 1:
                 break
+            if not os.path.isdir(os.path.join(data_dir, sub_dir)):
+                continue
             path_sub_dir = os.path.join(data_dir, sub_dir)
             all_files_in_dir = os.listdir(path_sub_dir)
             for file_type in all_files_in_dir:
                 for suffix in suffixes:
                     if file_type.endswith(suffix+NII_SUFFIX):
-                        cmd = '$FSLDIR/bin/fast -I 1 -v %s\n' % os.path.join(path_sub_dir, file_type)
-                        if cmd not in cmds:
+                        brain_name = file_type.split(NII_SUFFIX)[0]+'_brain'+NII_SUFFIX
+                        org_path = os.path.join(path_sub_dir, file_type)
+                        brain_path = os.path.join(path_sub_dir, brain_name)
+                        bet_cmd = "%s %s %s %s\n" % (BET_CMD, org_path, brain_path, BET_PARAMS)
+                        seg_cmd = "%s %s\n" % (SEG_CMD, brain_path)
+                        rm_cmd = "echo rm_%s; rm -rf %s/*pve* \n" % (file_type, path_sub_dir)
+                        if bet_cmd not in cmds and seg_cmd not in cmds and rm_cmd not in cmds:
                             cmds.append('echo Working on %s\n' % file_type)
-                            cmds.append(cmd)
+                            cmds.append(bet_cmd)
+                            cmds.append(seg_cmd)
+                            cmds.append(rm_cmd)
+
         f.writelines(cmds)
+    print("Done...")
+    print(cmd_file_out_path)
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='Create sh script for segmentation commands.')
