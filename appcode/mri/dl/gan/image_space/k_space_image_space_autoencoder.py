1e08a918a44940692c3569571a81ec90c27bce4e
ohadsh <shohad25@gmail.com>
2017-12-15 12:49:26 +0200

    Autoencdoer image space

---
+++ appcode/mri/dl/gan/image_space/k_space_image_space_autoencoder.py
@@ -0,0 +1,294 @@
+import tensorflow as tf
+import numpy as np
+from common.deep_learning.basic_model import BasicModel
+import common.deep_learning.ops as ops
+
+
+class KSpaceSuperResolutionWGAN(BasicModel):
+    """
+    Represents k-space super resolution model
+    """
+
+    def __init__(self, input=None, labels=None, dims_in=None, dims_out=None, FLAGS=None, train_phase=None, adv_loss_w=None):
+        """
+        :param input:
+        :param labels:
+        :param dims_in:
+        :param dims_out:
+        """
+        BasicModel.__init__(self, input=labels, labels=labels, dims_in=dims_in, dims_out=dims_out)
+        self.input = labels
+        print("Input is y label")
+        # HACK
+        self.input_d = labels
+        # HACK
+
+        self.FLAGS = FLAGS
+        self.train_phase = train_phase
+        self.predict_g = None
+        self.adv_loss_w = adv_loss_w
+
+        self.train_op_g = None
+        self.train_op_d = None
+
+        self.evaluation = None
+        self.update_ops = None
+        self.x_input_upscale = {}
+        self.debug = None
+        self.batch_size = self.FLAGS.mini_batch_size
+        # self.reg_w = self.FLAGS.regularization_weight
+        self.regularization_values = []
+        self.regularization_sum = None
+
+        self.regularization_values_d = []
+        self.regularization_sum_d = None
+
+        self.clip_weights = None
+
+        tf.get_collection('G')
+
+    def build(self):
+        """None
+        Build the model graph
+        :return:
+        """
+        with tf.name_scope('G_'):
+            self.predict_g = self.__G__()
+
+        with tf.name_scope('loss'):
+            # self.loss_g = self.__loss_g__(predict=self.predict_g, self.labels, reg=self.regularization_sum)
+            self.__loss__()
+
+        with tf.name_scope('training'):
+            self.train_op_g = self.__training__(learning_rate=self.FLAGS.learning_rate)
+
+        with tf.name_scope('evaluation'):
+            # Calculate accuracy L2 norm
+            self.evaluation = self.__evaluation__(predict=self.predict_g, labels=self.labels)
+
+    def __G__(self):
+        """
+        Define the model
+        """
+        x_real = self.input['real'] * self.input['mask']
+        x_imag = self.input['imag'] * self.input['mask']
+
+        # if self.FLAGS.gen_loss_adversarial > 0:
+        #     mask_not = tf.cast(tf.logical_not(tf.cast(self.labels['mask'], tf.bool)), tf.float32)
+        #     print "Noise level: (-0.01,0.01)"
+        #     minval = -0.01
+        #     maxval = 0.01
+        #     noise_real = mask_not * tf.random_uniform(shape=tf.shape(x_real), minval=minval, maxval=maxval,
+        #                                                 dtype=tf.float32, seed=None, name='z_real')
+        #     noise_imag = mask_not * tf.random_uniform(shape=tf.shape(x_real), minval=minval, maxval=maxval,
+        #                                                 dtype=tf.float32, seed=None, name='z_imag')
+        #     x_real += noise_real
+        #     x_imag += noise_imag
+
+        input_image = self.get_reconstructed_image(real=x_real, imag=x_imag, name='Both')
+        self.input_image = input_image
+
+        # Create the inputs
+        ref_image = self.get_reconstructed_image(real=self.input['real'], imag=self.input['imag'], name='Both')
+        ref_image = tf.abs(tf.complex(real=tf.expand_dims(ref_image[:,0,:,:], axis=1), imag=tf.expand_dims(ref_image[:,1,:,:], axis=1)))
+        tf.summary.image('G_reference', tf.transpose(ref_image, (0, 2, 3, 1)), collections='G', max_outputs=4)
+
+        if self.FLAGS.dump_debug:
+            zero_image = tf.abs(tf.complex(real=tf.expand_dims(input_image[:, 0, :, :], axis=1),
+                                          imag=tf.expand_dims(input_image[:, 1, :, :], axis=1)))
+            tf.summary.image('G_zero', tf.transpose(zero_image, (0, 2, 3, 1)), collections='G', max_outputs=4)
+
+        self.x_input_upscale['real'] = x_real
+        self.x_input_upscale['imag'] = x_imag
+
+        # Model convolutions
+        # Model convolutions
+        out_dim = 32
+        conv_1 = ops.conv2d(input_image, output_dim=out_dim, k_h=3, k_w=3, d_h=1, d_w=1, name="G1_conv_1")
+        conv_1_bn = ops.batch_norm(conv_1, self.train_phase, decay=0.98, name="G1_bn1")
+        relu_1 = tf.nn.relu(conv_1_bn, name='G1_relu1')
+
+        # Pool to 128x128
+        conv_2 = ops.conv2d(relu_1, output_dim=out_dim, k_h=3, k_w=3, d_h=1, d_w=1, name="G1_conv_2")
+        conv_2_bn = ops.batch_norm(conv_2, self.train_phase, decay=0.98, name="G1_bn2")
+        pool_2 = tf.layers.max_pooling2d(conv_2_bn, pool_size=[2, 2], strides=2, padding='same',
+                                         data_format='channels_first', name="G1_pool_2")
+        relu_2 = tf.nn.relu(pool_2, name='G1_relu2')
+
+        # Stay with 128x128
+        conv_3 = ops.conv2d(relu_2, output_dim=out_dim, k_h=3, k_w=3, d_h=1, d_w=1, name="G1_conv_3")
+        conv_3_bn = ops.batch_norm(conv_3, self.train_phase, decay=0.98, name="G1_bn3")
+        relu_3 = tf.nn.relu(conv_3_bn, name='G1_relu3')
+
+        # Pool to 64x64
+        conv_4 = ops.conv2d(relu_3, output_dim=out_dim, k_h=3, k_w=3, d_h=1, d_w=1, name="G1_conv_4")
+        conv_4_bn = ops.batch_norm(conv_4, self.train_phase, decay=0.98, name="G1_bn4")
+        pool_4 = tf.layers.max_pooling2d(conv_4_bn, pool_size=[2, 2], strides=2, padding='same',
+                                         data_format='channels_first', name="G1_pool_4")
+        relu_4 = tf.nn.relu(pool_4, name='G1_relu4')
+
+        # Stay with 64x64
+        conv_5 = ops.conv2d(relu_4, output_dim=out_dim, k_h=3, k_w=3, d_h=1, d_w=1, name="G1_conv_5")
+        conv_5_bn = ops.batch_norm(conv_5, self.train_phase, decay=0.98, name="G1_bn5")
+        relu_5 = tf.nn.relu(conv_5_bn, name='G1_relu5')
+
+        # Add noise GAN
+        # print "Noise level: (-0.05,0.05)"
+        # minval = -0.05
+        # maxval = 0.05
+        # noise = tf.random_uniform(shape=tf.shape(relu_5), minval=minval, maxval=maxval, dtype=tf.float32, seed=None,
+        #                           name='noise')
+        # relu_5 += noise
+
+        # From here, enlarge the network - decoder
+
+        # Enlarge to 128x128
+        out_shape = [self.batch_size, out_dim, 128, 128]
+        conv_6 = ops.conv2d_transpose(relu_5, output_shape=out_shape, k_h=3, k_w=3, d_h=2, d_w=2, name="G1_conv_6")
+        conv_6_bn = ops.batch_norm(conv_6, self.train_phase, decay=0.98, name="G1_bn6")
+        relu_6 = tf.nn.relu(conv_6_bn, name='G1_relu6')
+
+        # Stay with 128x128
+        conv_7 = ops.conv2d(relu_6, output_dim=out_dim, k_h=3, k_w=3, d_h=1, d_w=1, name="G1_conv_7")
+        conv_7_bn = ops.batch_norm(conv_7, self.train_phase, decay=0.98, name="G1_bn7")
+        relu_7 = tf.nn.relu(conv_7_bn, name='G1_relu7')
+
+        # Enlarge to 256x256
+        out_shape = [self.batch_size, out_dim, 256, 256]
+        conv_8 = ops.conv2d_transpose(relu_7, output_shape=out_shape, k_h=3, k_w=3, d_h=2, d_w=2, name="G1_conv_8")
+        conv_8_bn = ops.batch_norm(conv_8, self.train_phase, decay=0.98, name="G1_bn8")
+        relu_8 = tf.nn.relu(conv_8_bn, name='G1_relu8')
+
+        out_dim = 2
+        conv_last = ops.conv2d(relu_8, output_dim=out_dim, k_h=1, k_w=1, d_h=1, d_w=1, name="G1_conv_last")
+
+        predict = {}
+        predict['real'] = tf.reshape(conv_last[:,0,:,:], [-1, self.dims_out[0], self.dims_out[1], self.dims_out[2]], name='G_predict_real')
+        predict['imag'] = tf.reshape(conv_last[:,1,:,:], [-1, self.dims_out[0], self.dims_out[1], self.dims_out[2]], name='G_predict_imag')
+
+        # with tf.name_scope("final_predict"):
+        #     predict['real'] = tf.add(predict['real'], tf.expand_dims(input_image[:,0,:,:], axis=1), name='real')
+        #     predict['imag'] = tf.add(predict['imag'], tf.expand_dims(input_image[:,1,:,:], axis=1), name='imag')
+
+        tf.add_to_collection("predict", predict['real'])
+        tf.add_to_collection("predict", predict['imag'])
+
+        # Dump prediction out
+        out_image = tf.abs(tf.complex(real=predict['real'], imag=predict['imag']))
+        tf.summary.image('G_predict', tf.transpose(out_image, (0, 2, 3, 1)), collections='G', max_outputs=4)
+        return predict
+
+    def __loss__(self):
+        """
+        Calculate loss
+        :return:
+        """
+        # Context loss L2
+        label_real = tf.expand_dims(self.input_image[:,0,:,], axis=1)
+        label_imag = tf.expand_dims(self.input_image[:,1,:,], axis=1)
+        real_diff = tf.contrib.layers.flatten(self.predict_g['real'] - label_real)
+        imag_diff = tf.contrib.layers.flatten(self.predict_g['imag'] - label_imag)
+        print("You are using L2 loss")
+        self.context_loss = tf.reduce_mean(tf.square(real_diff) + tf.square(imag_diff), name='Context_loss_mean')
+
+        self.g_loss = self.FLAGS.gen_loss_context * self.context_loss
+        tf.summary.scalar('g_loss', self.g_loss, collections='G')
+
+        self.reg_loss_g = self.get_weights_regularization(dump=self.FLAGS.dump_debug, collection='G')
+        self.g_loss_no_reg = self.g_loss
+        self.g_loss += self.reg_loss_g
+        if self.FLAGS.dump_debug:
+            tf.summary.scalar('g_loss_plus_reg', self.g_loss, collections='G')
+            tf.summary.scalar('g_loss_reg_only', self.reg_loss_g, collections='D')
+
+    def __training__(self, learning_rate):
+        """
+        :param learning_rate:
+        :return:
+
+        """
+        t_vars = tf.trainable_variables()
+        self.g_vars = [var for var in t_vars if 'G_' in var.name]
+
+        # Create RMSProb optimizer with the given learning rate.
+        optimizer_g = tf.train.RMSPropOptimizer(self.FLAGS.learning_rate, centered=True)
+
+        # Create a variable to track the global step.
+        global_step_g = tf.Variable(0, name='global_step_g', trainable=False)
+
+        # Use the optimizer to apply the gradients that minimize the loss
+        # (and also increment the global step counter) as a single training step.
+        grad_g = optimizer_g.compute_gradients(loss=self.g_loss, var_list=self.g_vars)
+
+        self.update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+
+        # Ensures that we execute the update_ops before performing the train_step
+        with tf.control_dependencies(self.update_ops):
+            train_op_g = optimizer_g.apply_gradients(grad_g, global_step=global_step_g)
+
+        return train_op_g
+
+    def __evaluation__(self, predict, labels):
+        """
+        :param predict:
+        :param labels:
+        :return:
+        """
+        evalu = self.context_loss
+        return evalu
+
+    def get_reconstructed_image(self, real, imag, name=None):
+        """
+        :param real:
+        :param imag:
+        :param name:
+        :return:
+        """
+        complex_k_space_label = tf.complex(real=tf.squeeze(real), imag=tf.squeeze(imag), name=name+"_complex_k_space")
+        rec_image_complex = tf.expand_dims(tf.ifft2d(complex_k_space_label), axis=1)
+        
+        rec_image_real = tf.reshape(tf.real(rec_image_complex), shape=[-1, 1, self.dims_out[1], self.dims_out[2]])
+        rec_image_imag = tf.reshape(tf.imag(rec_image_complex), shape=[-1, 1, self.dims_out[1], self.dims_out[2]])
+
+        # Shifting
+        top, bottom = tf.split(rec_image_real, num_or_size_splits=2, axis=2)
+        top_left, top_right = tf.split(top, num_or_size_splits=2, axis=3)
+        bottom_left, bottom_right = tf.split(bottom, num_or_size_splits=2, axis=3)
+
+        top_shift = tf.concat(axis=3, values=[bottom_right, bottom_left])
+        bottom_shift = tf.concat(axis=3, values=[top_right, top_left])
+        shifted_image = tf.concat(axis=2, values=[top_shift, bottom_shift])
+
+        # Shifting
+        top_imag, bottom_imag = tf.split(rec_image_imag, num_or_size_splits=2, axis=2)
+        top_left_imag, top_right_imag = tf.split(top_imag, num_or_size_splits=2, axis=3)
+        bottom_left_imag, bottom_right_imag = tf.split(bottom_imag, num_or_size_splits=2, axis=3)
+
+        top_shift_imag = tf.concat(axis=3, values=[bottom_right_imag, bottom_left_imag])
+        bottom_shift_imag = tf.concat(axis=3, values=[top_right_imag, top_left_imag])
+        shifted_image_imag = tf.concat(axis=2, values=[top_shift_imag, bottom_shift_imag])
+
+        shifted_image_two_channels = tf.stack([shifted_image[:,0,:,:], shifted_image_imag[:,0,:,:]], axis=1)
+        return shifted_image_two_channels
+
+    def get_weights_regularization(self, dump=False, collection=None):
+        """
+        Calculate sum of regularization (L2)
+        :param dump: dump to tensorboard
+        :return:
+        """
+        if collection is None:
+            w_collection = tf.get_collection('regularization_w')
+            b_collection = tf.get_collection('regularization_b')
+        else:
+            w_collection = [var for var in tf.get_collection('regularization_w') if collection in var.name]
+            b_collection = [var for var in tf.get_collection('regularization_b') if collection in var.name]
+
+        reg_w = tf.add_n(w_collection, name='regularization_w') if len(w_collection) > 0 else 0
+        reg_b = tf.add_n(b_collection, name='regularization_b') if len(b_collection) > 0 else 0
+
+        if dump:
+            tf.summary.scalar('Regularization - W', reg_w, collections=collection)
+            tf.summary.scalar('Regularization - b', reg_b, collections=collection)
+
+        return self.FLAGS.reg_w * reg_w + self.FLAGS.reg_b * reg_b
